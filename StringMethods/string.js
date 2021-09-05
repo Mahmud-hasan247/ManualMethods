c68f83394f6181b9330205e90426932647a97d71
Mahmud Hasan <mahmudhasan5406@gmail.com>
2021-09-05 17:43:27 +0600

    string methods

---
+++ StringMethods/string.js
@@ -0,0 +1,28 @@
+const _1 = "Anchor"; // This Method for create html <a> tag.
+const _2 = "At"; // This method for access element in a string.
+const _3 = "Big"; // This method for create html <big> tag, which is removed from html5.
+const _4 = "Blink"; // This method for showing a string in html <blink> tag.
+const _5 = "Bold"; // This method for creating a html <b> tag.
+const _6 = "charAt"; // This method for accessing element from a string.
+const _7 = "charCodeAt"; // This method for getting the code of a character between 0 and 65535
+const _8 = "codePointAt"; // This method for getting the code of a character between 0 and 65535
+const _9 = "concat"; // This method for concat two string. it accepts two parameter.
+const _10 = "constructor"; // need R&D
+const _11 = "endsWith"; // This method for checking a string that ends with a specific character, word or sentence.
+const _12 = "fixed"; // This method for creating teletype <tt> text in html.
+const _13 = "fontcolor"; // This method to create <font> html tag and specify the color.
+const _14 = "fontsize"; // This method to create <font> html tag and specify the font size.
+const _15 = "includes"; // This method to check a word, character or sentence is included or not in a string.
+const _16 = "indexOf" // This method to getting the index number of a character or element in a string.
+const _17 = "italics" // This method to create <i> html tag.
+
+
+// similar methods
+// category one
+// _1 at
+// _2 charAt
+// _3 element access by bracket notation
+
+// category two
+// _1 charCodeAt
+// _2 codePointAt
