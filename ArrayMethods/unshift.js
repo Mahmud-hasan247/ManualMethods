62635fda40680db121feb982996e9fd93a259127
Mahmud Hasan <mahmudhasan5406@gmail.com>
2021-09-14 12:53:31 +0600

    another solution for unshift

--- ArrayMethods/unshift.js
+++ ArrayMethods/unshift.js
@@ -1,3 +1,4 @@
+// solution - 1
 const arr = [2, 3, 4, 5];
 
 const AddElementInFirst = (arr, newElement) => {
@@ -7,3 +8,16 @@ const AddElementInFirst = (arr, newElement) => {
 
 const result = AddElementInFirst(arr, 1);
 console.log(result); // expected: [ 1, 2, 3, 4, 5 ]
+
+// solution - 2
+const LetsUnshift = function (array, item) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    array[i + 1] = array[i];
+  }
+  array[0] = item;
+  return array;
+};
+
+const array = [7, 8, 9];
+const Result = LetsUnshift(array, 6);
+console.log(Result); // expected: [ 6, 7, 8, 9 ]
