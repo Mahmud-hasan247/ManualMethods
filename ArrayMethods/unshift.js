5060cc0dbafd1b1e742e40e211fd0c586056b9f6
Mahmud Hasan <mahmudhasan5406@gmail.com>
2021-09-14 12:43:44 +0600

    unshift

--- ArrayMethods/unshift.js
+++ ArrayMethods/unshift.js
@@ -1,12 +1,9 @@
-// const arr = [2, 3, 4, 5];
+const arr = [2, 3, 4, 5];
 
-// const AddElementInFirst = (arr, newElement) => {
-//   for (let i = 0; i < 1; i++) {
-//     arr[i] = arr[i];
-//   }
-//   arr[0] = newElement;
-//   return arr;
-// };
+const AddElementInFirst = (arr, newElement) => {
+  arr.reverse()[arr.length] = newElement;
+  return arr.reverse();
+};
 
-// const result = AddElementInFirst(arr, 1);
-// console.log(result); // expected: [ 1, 2, 3, 4, 5 ]
+const result = AddElementInFirst(arr, 1);
+console.log(result); // expected: [ 1, 2, 3, 4, 5 ]
